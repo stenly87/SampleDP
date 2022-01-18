e220fc18cf406d267656fc040b5de5b15e3900aa
ppk <ppk_student@hotmail.com>
2022-01-19 09:43:22 +1000

    upd

--- WpfApp10/Group.cs
+++ WpfApp10/Group.cs
@@ -5,7 +5,7 @@ namespace WpfApp10
     public class Group
     {
         public string Title { get; set; }
-        public DateTime YearJoin { get; set; }
+        public int YearJoin { get; set; }
         public Special Special { get; set; }
     }
 }
